2b731b7216809b5af1a0e4127280f9cfa92c0236
aqa210 <24020013@vnu.edu.vn>
2025-09-25 10:49:03 +0700

    Add Introduction.java for HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,15 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Nguyễn An Quốc Anh\t24020013\tIT4-k69\taqua210\t24020013@vnu.edu.vn");
+        for (int i = 9; i >= 1; i--) {
+            if (i > 1) {
+                System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+                System.out.println("Take one down, pass it around,");
+            } else {
+                System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+                System.out.println("Take one down, pass it around,");
+                System.out.println("No more bottles of beer on the wall.");
+            }
+        }
+    }
+}
