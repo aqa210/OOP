53d0c2ec31dc931efead14f30f85812f635ec71e
aqa210 <24020013@vnu.edu.vn>
2025-09-25 14:25:19 +0700

    nộp bài

---
+++ HW1/Sumoftwodice.java
@@ -0,0 +1,8 @@
+public class Sumoftwodice{
+    public static void main(String[] args){
+        int sides=6;
+        int a=1+(int)Math.random()*sides;
+        int b=1+(int)Math.random()*sides;
+        System.out.println(a+b);
+    }
+}
