64ad2b8d5ff98db00d481bfe96924394e7d65e80
aqa210 <24020013@vnu.edu.vn>
2025-10-03 00:01:42 +0700

    homework2

--- HW2/src/main/java/CharGrid.java
+++ HW2/src/main/java/CharGrid.java
@@ -1,34 +1,65 @@
-// HW1 2-d array Problems
-// CharGrid encapsulates a 2-d grid of chars and supports
-// a few operations on the grid.
+public class CharGrid{
+    private char [][] grid;
+    public CharGrid(char grid [][]){
+        this.grid = grid;
+    }
+    public int charArea(char ch){
+        int mirow=Integer.MAX_VALUE;
+        int marow=Integer.MIN_VALUE;
+        int micol=Integer.MAX_VALUE;
+        int macol=Integer.MIN_VALUE;
+        int c=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]==ch){
+                    mirow=Math.min(mirow,i);
+                    marow=Math.max(marow,i);
+                    micol=Math.min(micol,j);
+                    macol=Math.max(macol,j);
+                    c++;
+                }
+            }
+        }
+        if(mirow==Integer.MAX_VALUE) return 0;
+        if(c==1) return c;
+        return (marow-mirow+1)*(macol-micol+1);
+    }
+    public int countPlus() {
+        int c=0;
+        int hang=grid.length;
+        int cot=grid[0].length;
+        for(int i=0;i<hang;i++){
+            for(int j=0;j<cot;j++){
+                int tren=dodai(i,j,-1,0,grid[i][j]);
+                int duoi=dodai(i,j,1,0,grid[i][j]);
+                int trai=dodai(i,j,0,-1,grid[i][j]);
+                int phai=dodai(i,j,0,1,grid[i][j]);
+                if(tren == duoi && trai == phai && tren == trai && tren >= 1) {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
 
-public class CharGrid {
-	private char[][] grid;
+    // Hàm private tính chiều dài nhánh theo hướng
+    private int dodai(int i, int j, int dx, int dy, char ch) {
+        int len = 0;
+        int numRows = grid.length;
+        int numCols = grid[0].length;
 
-	/**
-	 * Constructs a new CharGrid with the given grid.
-	 * Does not make a copy.
-	 * @param grid
-	 */
-	public CharGrid(char[][] grid) {
-		this.grid = grid;
-	}
-	
-	/**
-	 * Returns the area for the given char in the grid. (see handout).
-	 * @param ch char to look for
-	 * @return area for given char
-	 */
-	public int charArea(char ch) {
-		return 0; // YOUR CODE HERE
-	}
-	
-	/**
-	 * Returns the count of '+' figures in the grid (see handout).
-	 * @return number of + in grid
-	 */
-	public int countPlus() {
-		return 0; // YOUR CODE HERE
-	}
-	
+        int x = i + dx;
+        int y = j + dy;
+
+        while (x >= 0 && x < numRows && y >= 0 && y < numCols
+                && grid[x][y] == ch) {
+            len++;
+            x += dx;
+            y += dy;
+        }
+        return len;
+    }
+
 }
+
+
--- HW2/src/main/java/StringCode.java
+++ HW2/src/main/java/StringCode.java
@@ -1,39 +1,49 @@
 import java.util.HashSet;
-import java.util.Set;
 
-// CS108 HW1 -- String static methods
+public class StringCode{
+    String blowup(String str){
+        String ans = "";
+        int c=0;
+        for(int i=0;i<str.length();i++){
+            while(c>0){
+                ans+=str.charAt(i);
+                c--;
+            }
+            if(str.charAt(i)>='0'&&str.charAt(i)<='9'){
+                c=str.charAt(i)-'0';
+                continue;
+            }
+            ans+=str.charAt(i);
+        }
+        return ans;
+    }
+    int maxRun(String str){
+        int c=1;
+        int ans=1;
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==(str.charAt(i-1))){
+                c++;
+                ans=Math.max(c,ans);
+            }
+            else{
+                ans=Math.max(c,ans);
+                c=1;
+            }
+        }
+        return ans;
+    }
+    boolean stringIntersect(String a, String b, int len){
+        HashSet <String> hset = new HashSet<>();
+        for(int i=0;i<a.length()-len;i++){
+            hset.add(a.substring(i,i+len));
+        }
+        for(int i=0;i<b.length()-len;i++){
+            if(hset.contains(b.substring(i,i+len))){
+                return true;
+            }
+        }
+        return false;
+    }
 
-public class StringCode {
 
-	/**
-	 * Given a string, returns the length of the largest run.
-	 * A a run is a series of adajcent chars that are the same.
-	 * @param str
-	 * @return max run length
-	 */
-	public static int maxRun(String str) {
-		return 0; // YOUR CODE HERE
-	}
-
-	
-	/**
-	 * Given a string, for each digit in the original string,
-	 * replaces the digit with that many occurrences of the character
-	 * following. So the string "a3tx2z" yields "attttxzzz".
-	 * @param str
-	 * @return blown up string
-	 */
-	public static String blowup(String str) {
-		return null; // YOUR CODE HERE
-	}
-	
-	/**
-	 * Given 2 strings, consider all the substrings within them
-	 * of length len. Returns true if there are any such substrings
-	 * which appear in both strings.
-	 * Compute this in linear time using a HashSet. Len will be 1 or more.
-	 */
-	public static boolean stringIntersect(String a, String b, int len) {
-		return false; // YOUR CODE HERE
-	}
 }
